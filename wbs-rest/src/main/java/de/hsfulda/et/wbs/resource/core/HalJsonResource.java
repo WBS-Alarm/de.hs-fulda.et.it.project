7e12dee4291ed50d1c326f43294cbfb553dae6d5
Achim Rose <achim.rose@gmail.com>
2019-02-27 05:25:10 +0100

    Tests und ein wenig Refactoring #3

--- wbs-rest/src/main/java/de/hsfulda/et/wbs/resource/core/HalJsonResource.java
+++ wbs-rest/src/main/java/de/hsfulda/et/wbs/resource/core/HalJsonResource.java
@@ -41,6 +41,17 @@ public class HalJsonResource {
         return embedded;
     }
 
+
+    /**
+     * Liefert die Map zurück in der die Eigenschaften der HalJsonResource enthalten sind.
+     *
+     * @return Liefert die Map zurück in der die Eigenschaften der HalJsonResource enthalten sind.
+     */
+    @JsonAnyGetter
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
     /**
      * Fügt einen neuen Link auf die bestehende Liste hinzu.
      *
@@ -54,40 +65,9 @@ public class HalJsonResource {
         links.add(link);
     }
 
-    /**
-     * Die Methode sucht Links der Resource heraus, die mit der gesuchten Relation übereinstimmen.
-     *
-     * @param relation Relation nach der der Link gesucht werden soll.
-     * @return Liefert eine Liste von Links zurück, die mit der Relation übereinstimmen ansonsten wird eine leere Liste
-     * zurück gegeben.
-     */
-    public List<Link> getLinks(final String relation) {
-        Objects.requireNonNull(relation, "Relation must not be null");
-
-        return Collections.unmodifiableList(getLinksForRelation(relation));
-    }
 
     private List<Link> getLinksForRelation(final String relation) {
         return links.computeIfAbsent(relation, k -> new ArrayList<>());
-    }
-
-    /**
-     * Die Methode sucht einen Link aus der Linkliste heraus, in der die Relation übereinstimmt.
-     * <strong>Vorsicht:</strong> Es wird nur die erste Übereinstimmung gewählt.
-     *
-     * @param relation Relation nach der der Link gesucht werden soll.
-     * @return Liefert den ersten Link zurück, mit der die Relation übereinstimmt ansonsten {@code null}.
-     */
-    public Link getLink(final String relation) {
-        Objects.requireNonNull(relation, "Relation must not be null");
-
-        List<Link> links = getLinksForRelation(relation);
-
-        if (links.size() > 0) {
-            return links.get(0);
-        }
-
-        return null;
     }
 
     /**
@@ -141,106 +121,4 @@ public class HalJsonResource {
 
         properties.put(name, value);
     }
-
-    /**
-     * Liefert die Map zurück in der die Eigenschaften der HalJsonResource enthalten sind.
-     *
-     * @return Liefert die Map zurück in der die Eigenschaften der HalJsonResource enthalten sind.
-     */
-    @JsonAnyGetter
-    public Map<String, Object> getProperties() {
-        return properties;
-    }
-
-    /**
-     * Gibt an, ob das Property mit dem übergebenen Namen existiert. Die Properties "_links" und "_embedded" gelten
-     * jeweils als existierend, wenn Links bzw. eingebette Ressourcen tatsächlich existieren.
-     *
-     * @param name Der Name des Properties.
-     * @return <code>true</code>, wenn das Property existiert, sonst <code>false</code>.
-     */
-    public boolean hasProperty(final String name) {
-        requireNonNull(name);
-        if (name.equals(_LINKS)) {
-            return !links.isEmpty();
-        }
-        if (name.equals(_EMBEDDED)) {
-            return !embedded.isEmpty();
-        }
-        return properties.containsKey(name);
-    }
-
-    /**
-     * Liefert den Wert des Properties mit dem übergebenen Namen zurück.
-     * <p>
-     * Die Properties "_links" und "_embedded" werden leicht anders behandelt. Sie werden als unmodifiableMap zurück
-     * gegeben. Um konsistent mit hasProperty() zu sein, wird <code>null</code> geliefert, falls hasProperty()
-     * <code>false</code> liefert.
-     *
-     * @param name Der Name des Properties.
-     * @return <code>true</code>, wenn das Property existiert, sonst <code>false</code>.
-     */
-    public Object getProperty(final String name) {
-        requireNonNull(name);
-        if (name.equals(_LINKS)) {
-            return getAsProperty(links);
-        }
-        if (name.equals(_EMBEDDED)) {
-            return getAsProperty(embedded);
-        }
-        return properties.get(name);
-    }
-
-    private Map<?, ?> getAsProperty(final Map<?, ?> internalMap) {
-        if (internalMap.isEmpty()) {
-            return null;
-        }
-        return Collections.unmodifiableMap(internalMap);
-    }
-
-    /**
-     * Liefert den einzigen Link mit der angegeben Relation und dem angegebenen Namen zurück.
-     *
-     * @param relation Die Relation des gesuchten Links.
-     * @param name Der Name des gesuchten Links.
-     * @return Der gesuchte Link.
-     * @throws IllegalArgumentException falls es mehrere oder keinen passenden Link gibt.
-     */
-    public NamedLink getLink(final String relation, final String name) {
-        requireNonNull(name);
-        List<NamedLink> namedLinks = filterLinksNamed(name, getLinks(relation));
-        checkExcatlyOneLinkFound(namedLinks, relation, name);
-        return namedLinks.get(0);
-    }
-
-    private List<NamedLink> filterLinksNamed(final String name, final List<Link> links) {
-        List<NamedLink> namedLinks = new ArrayList<>();
-        for (Link link : links) {
-            addLinkIfNamed(name, namedLinks, link);
-        }
-        return namedLinks;
-    }
-
-    private void checkExcatlyOneLinkFound(final List<NamedLink> namedLinks, final String relation, final String name) {
-        if (namedLinks.size() > 1) {
-            throw new IllegalArgumentException("Mehr als einen Link mit rel="
-                + relation
-                + " und name="
-                + name
-                + " gefunden");
-        }
-        if (namedLinks.isEmpty()) {
-            throw new IllegalArgumentException("Keinen Link mit rel=" + relation + " und name=" + name + " gefunden.");
-        }
-    }
-
-    private void addLinkIfNamed(final String name, final List<NamedLink> namedLinks, final Link link) {
-        if (link instanceof NamedLink) {
-            NamedLink namedLink = (NamedLink) link;
-            if (name.equals(namedLink.getName())) {
-                namedLinks.add(namedLink);
-            }
-        }
-    }
-
 }
