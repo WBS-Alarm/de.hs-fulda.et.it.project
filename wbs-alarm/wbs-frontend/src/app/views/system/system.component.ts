0c675af9fc284b725f8911f292f77ad5d6149188
Tim Wieder <tim.wieder@plentymarkets.com>
2019-04-04 09:25:47 +0200

    styling

--- wbs-alarm/wbs-frontend/src/app/views/system/system.component.ts
+++ wbs-alarm/wbs-frontend/src/app/views/system/system.component.ts
@@ -1,11 +1,12 @@
-import {AfterViewInit, Component, OnInit} from "../../../../node_modules/@angular/core";
-import {TerraNodeInterface, TerraNodeTreeConfig} from "@plentymarkets/terra-components";
-import {TranslationService} from "angular-l10n";
-import {CarrierService} from "../../core/service/rest/carrier/carrier.service";
-import {Observable} from "rxjs/observable";
 import {
-    tap
-} from 'rxjs/operators';
+    Component,
+    OnInit
+} from '../../../../node_modules/@angular/core';
+import { TerraNodeTreeConfig } from "@plentymarkets/terra-components";
+import { TranslationService } from "angular-l10n";
+import { CarrierService } from "../../core/service/rest/carrier/carrier.service";
+import { Observable } from "rxjs/observable";
+import { tap } from 'rxjs/operators';
 
 export interface resultData
 {
@@ -33,15 +34,10 @@ export interface ExampleTreeData
 @Component({
     selector: 'system',
     template: require('./system.component.html'),
-    styles: [require('./system.component.scss')]
+    styles:   [require('./system.component.scss')]
 })
 export class SystemComponent implements OnInit
 {
-    private nodeCounter:number = 0;
-
-    private carriers:any;
-
-
     constructor(private nodeTreeConfig:TerraNodeTreeConfig<ExampleTreeData>,
                 private translation:TranslationService,
                 private carrierService:CarrierService)
@@ -52,76 +48,6 @@ export class SystemComponent implements OnInit
     {
         this.createCompleteTree();
     }
-
-
-    // protected addNode():void
-    // {
-    //     this.nodeTreeConfig.addNode({
-    //         name:      'Test' + this.nodeCounter,
-    //         id:        this.nodeCounter,
-    //         isVisible: true
-    //     });
-    //
-    //     this.nodeCounter++;
-    // }
-    //
-    // protected addExistingNode():void
-    // {
-    //     this.nodeTreeConfig.addNode({
-    //         name: 'Test' + this.nodeCounter,
-    //         id:   0
-    //     });
-    // }
-    //
-    // protected findNodeById(id:string | number):void
-    // {
-    //     let node:TerraNodeInterface<ExampleTreeData> = this.nodeTreeConfig.findNodeById(id);
-    //     alert(node.name);
-    //
-    // }
-
-    // protected deleteNodeById(id:string | number):void
-    // {
-    //     this.nodeTreeConfig.removeNodeById(id);
-    // }
-    //
-    // protected getSelectedNode():void
-    // {
-    //     console.log(this.nodeTreeConfig.currentSelectedNode);
-    // }
-    //
-    // protected deleteSelectedNode():void
-    // {
-    //     this.nodeTreeConfig.removeNode(this.nodeTreeConfig.currentSelectedNode);
-    // }
-    //
-    // protected updateSelectedNode():void
-    // {
-    //     this.nodeTreeConfig.currentSelectedNode.name = 'Terra';
-    // }
-    //
-    // protected updateNodeById(id:string | number):void
-    // {
-    //     this.nodeTreeConfig.updateNodeById(id,
-    //         {
-    //             id:   id,
-    //             name: 'Terra'
-    //         });
-    // }
-    //
-    // protected addChildToNodeById(id:string | number):void
-    // {
-    //     this.nodeTreeConfig.addChildToNodeById(id, {
-    //         id:        133,
-    //         name:      'myNewNode',
-    //         isVisible: true
-    //     });
-    // }
-    //
-    // protected setSelectedNode(id:string | number):void
-    // {
-    //     this.nodeTreeConfig.setCurrentSelectedNodeById(id);
-    // }
 
     protected createCompleteTree():void
     {
@@ -140,18 +66,18 @@ export class SystemComponent implements OnInit
                             name:      'Subchild1',
                             isVisible: true,
                             onClick:   ():void =>
-                            {
-                                alert('Hello i am a click function');
-                            }
+                                       {
+                                           alert('Hello i am a click function');
+                                       }
                         }]
                     }
                 ]
             },
             {
-                id: 21,
-                name: this.translation.translate('system.village.village'),
-                isVisible: true,
-                children: [],
+                id:         21,
+                name:       this.translation.translate('system.village.village'),
+                isVisible:  true,
+                children:   [],
                 onLazyLoad: ():Observable<any> =>
                             {
                                 return this.getCarriers();
@@ -168,8 +94,8 @@ export class SystemComponent implements OnInit
                     result._embedded.elemente.forEach((element:any) =>
                     {
                         this.nodeTreeConfig.addChildToNodeById(21, {
-                            id: element.id,
-                            name: element.name,
+                            id:        element.id,
+                            name:      element.name,
                             isVisible: true
                         });
                     })
