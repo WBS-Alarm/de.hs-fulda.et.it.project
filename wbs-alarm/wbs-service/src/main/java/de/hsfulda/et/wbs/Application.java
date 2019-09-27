d923effaab45c5f10524267a1417259203d6b386
AAchim Rose <achim.rose@gmail.com>
2019-09-27 14:28:23 +0200

    Umstellung Tomcat Deployment

--- wbs-alarm/wbs-service/src/main/java/de/hsfulda/et/wbs/Application.java
+++ wbs-alarm/wbs-service/src/main/java/de/hsfulda/et/wbs/Application.java
@@ -2,13 +2,21 @@ package de.hsfulda.et.wbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static final String CONTEXT_ROOT = "/wbs";
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args); // :8080
+    }
+
+    // Override the configure method from the SpringBootServletInitializer class
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
