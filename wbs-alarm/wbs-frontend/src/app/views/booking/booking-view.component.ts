4849f7dee76b146269259e75d3f3819f47058bdb
Tim Wieder <tmwieder@gmail.com>
2019-10-16 13:30:19 +0200

    erste version der booking view

--- wbs-alarm/wbs-frontend/src/app/views/booking/booking-view.component.ts
+++ wbs-alarm/wbs-frontend/src/app/views/booking/booking-view.component.ts
@@ -2,6 +2,7 @@ import {
     Component,
     OnInit
 } from '@angular/core';
+import { TerraSelectBoxValueInterface } from '@plentymarkets/terra-components';
 
 @Component({
     selector: 'booking',
@@ -10,6 +11,12 @@ import {
 })
 export class BookingViewComponent implements OnInit
 {
+    public _traeger:string = 'Eschenstruth';
+    public _zielorte:Array<TerraSelectBoxValueInterface> = [];
+    public _von:TerraSelectBoxValueInterface;
+    public _nach:TerraSelectBoxValueInterface;
+    public _modus:string ='buchen';
+
     constructor()
     {
 
@@ -17,5 +24,20 @@ export class BookingViewComponent implements OnInit
 
     public ngOnInit():void
     {
+        this._zielorte = [
+            {
+                value: 1,
+                caption: 'Eschenstruth'
+        },
+            {
+                value: 2,
+                caption: 'Helsa'
+            }]
+    }
+
+    public _buchen():void
+    {
+        console.log('von:' + this._von);
+        console.log('nach:' + this._nach);
     }
 }
