53ff1b35bebe5dae8f327b6ea580a47adc688b0c
AAchim Rose <achim.rose@gmail.com>
2019-10-18 13:14:44 +0200

    Test zum Ansprechen der Transaktionresource

--- wbs-alarm/wbs-service/src/test/java/de/hsfulda/et/wbs/http/resource/TransaktionResourceTest.java
+++ wbs-alarm/wbs-service/src/test/java/de/hsfulda/et/wbs/http/resource/TransaktionResourceTest.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Die Transaktion Resource")
@@ -31,5 +34,18 @@ class TransaktionResourceTest extends ResourceTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @DisplayName("JSON kann interpretiert werden, invalide Daten.")
+    @Test
+    void putAendernEinesZielorts() throws Exception {
+        Long traegerId = getTraegerId(HE_TRAEGER);
+        mockMvc.perform(post(TransaktionListResource.PATH, traegerId)
+                .header("Authorization", getToken("HelsaBuchung"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"von\":-12,\"nach\":-10,\"positions\":[{\"groesse\":-1,\"anzahl\":1}]}"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", is("Zielort mit ID -12 nicht gefunden.")));
     }
 }
