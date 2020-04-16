ba5a274accac7671a16ccf062a919652bc6e33e2
WiederT <tmwieder@gmail.com>
2020-04-16 10:32:25 +0200

    - added table for bericht

--- wbs-alarm/wbs-frontend/src/app/views/reports/bericht-view.component.ts
+++ wbs-alarm/wbs-frontend/src/app/views/reports/bericht-view.component.ts
@@ -1,8 +1,18 @@
 import {Component, OnInit} from "@angular/core";
 import {GlobalRegistryService} from "../../core/global-registry/global-registry.service";
-import {Observable} from "rxjs";
 import {ActivatedRoute, Data, Route} from "@angular/router";
 import {ReportService} from "./service/report.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {Observable} from "rxjs";
+
+export interface BerichtRow
+{
+    traeger:any;
+    kategorie:any;
+    groesse:any;
+    zielort:any;
+    anzahl:any;
+}
 
 @Component({
     selector: 'bericht-view',
@@ -15,6 +25,11 @@ export class BerichtViewComponent implements OnInit
     public routeData$:Observable<Data>;
     public trager:any;
     public traegers:Array<any>;
+
+    private tableData:Array<BerichtRow> = [];
+
+    public displayedColumns:Array<string> = ['traeger', 'kategorie', 'größe', 'zielort', 'anzahl'];
+    public dataSource:MatTableDataSource<BerichtRow> = new MatTableDataSource<BerichtRow>(this.tableData);
 
     constructor(private globalRegistryService:GlobalRegistryService,
                 private route:ActivatedRoute,
@@ -38,7 +53,18 @@ export class BerichtViewComponent implements OnInit
     {
         this.berichtService.listBestaendeZuTraeger(this.trager.id).subscribe((bericht:any) =>
         {
+            bericht._embedded.elements.forech((element:any) =>
+            {
+                this.tableData.push({
+                    traeger: element.traeger,
+                    kategorie: element.kategorie,
+                    groesse: element.groesse,
+                    zielort: element.zielort,
+                    anzahl: element.anzahl,
+                });
+            });
 
+            this.dataSource._updateChangeSubscription();
         });
     }
 
