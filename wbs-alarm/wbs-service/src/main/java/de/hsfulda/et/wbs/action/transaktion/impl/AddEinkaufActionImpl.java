267a52f7b2c328559fddfd5a7a47ecd710ec18bf
Achim Rose <achim.rose@gmail.com>
2020-05-08 11:33:46 +0200

    Korrektur Dependencies, Absicherung Einkauf

--- wbs-alarm/wbs-service/src/main/java/de/hsfulda/et/wbs/action/transaktion/impl/AddEinkaufActionImpl.java
+++ wbs-alarm/wbs-service/src/main/java/de/hsfulda/et/wbs/action/transaktion/impl/AddEinkaufActionImpl.java
@@ -6,6 +6,7 @@ import de.hsfulda.et.wbs.core.WbsUser;
 import de.hsfulda.et.wbs.core.data.TransaktionData;
 import de.hsfulda.et.wbs.core.dto.PositionDto;
 import de.hsfulda.et.wbs.repository.ZielortRepository;
+import de.hsfulda.et.wbs.service.AccessService;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
@@ -17,23 +18,27 @@ public class AddEinkaufActionImpl implements AddEinkaufAction {
 
     private final ZielortRepository zielorte;
     private final AddTransaktionAction addAction;
+    private final AccessService accessService;
 
-    public AddEinkaufActionImpl(ZielortRepository zielorte, AddTransaktionAction addAction) {
+    public AddEinkaufActionImpl(ZielortRepository zielorte, AddTransaktionAction addAction,
+            AccessService accessService) {
         this.zielorte = zielorte;
         this.addAction = addAction;
+        this.accessService = accessService;
     }
 
     @Override
     public TransaktionData perform(WbsUser user, Long tragerId, List<PositionDto> dto) {
+        return accessService.hasAccessOnTraeger(user, tragerId, () -> {
+            Long wareneingangId = zielorte.findWareneingangByTraegerId(tragerId)
+                    .orElseThrow(() -> new IllegalStateException(
+                            "Fehler beim Bezug des Wareneingangs. Bitte wenden Sie sich an Ihren Systemadministrator."));
 
-        Long wareneingangId = zielorte.findWareneingangByTraegerId(tragerId)
-                .orElseThrow(() -> new IllegalStateException(
-                        "Fehler beim Bezug des Wareneingangs. Bitte wenden Sie sich an Ihren Systemadministrator."));
+            Long lagerId = zielorte.findLagerByTraegerId(tragerId)
+                    .orElseThrow(() -> new IllegalStateException(
+                            "Fehler beim Bezug des Lagers. Bitte wenden Sie sich an Ihren Systemadministrator."));
 
-        Long lagerId = zielorte.findLagerByTraegerId(tragerId)
-                .orElseThrow(() -> new IllegalStateException(
-                        "Fehler beim Bezug des Lagers. Bitte wenden Sie sich an Ihren Systemadministrator."));
-
-        return addAction.perform(user, TransaktionDtoImpl.of(wareneingangId, lagerId, dto));
+            return addAction.perform(user, TransaktionDtoImpl.of(wareneingangId, lagerId, dto));
+        });
     }
 }
