23f8511d1efd3d4d1931e95316a9a1010dc385ee
WiederT <tmwieder@gmail.com>
2020-12-03 16:09:28 +0100

    - added functioning test to booking view

--- wbs-alarm/wbs-frontend/src/app/views/booking/booking-view.component.spec.ts
+++ wbs-alarm/wbs-frontend/src/app/views/booking/booking-view.component.spec.ts
@@ -1,7 +1,6 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
-import {LoginService} from "../../core/service/rest/login/login.service";
 import {UsersService} from "../../core/service/rest/users/users.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Data, Router} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {LocalizationModule} from "angular-l10n";
 import {l10nConfig} from "../../core/localization/l10n.config";
@@ -10,23 +9,63 @@ import {WbsSitemapHelper} from "../../core/service/rest/sitemap/data/wbs-sitemap
 import {GlobalRegistryService} from "../../core/global-registry/global-registry.service";
 import {AlertService, TerraComponentsModule} from "@plentymarkets/terra-components";
 import {BookingViewComponent} from "./booking-view.component";
+import {CategoryService} from "../../core/service/rest/categories/category.service";
+import {CarrierService} from "../../core/service/rest/carrier/carrier.service";
+import {TransaktionService} from "../../core/service/rest/transaktions/transaktion.service";
+import {MatTableModule} from "@angular/material/table";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatRadioModule} from "@angular/material/radio";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Observable} from "rxjs";
 
-describe('Component: AppLoginComponent', () => {
+describe('Component: BookingViewComponent', () => {
     let component: BookingViewComponent;
     let fixture: ComponentFixture<BookingViewComponent>;
 
-    const loginServiceStub: Partial<LoginService> = {};
-    const usersServiceStub: Partial<UsersService> = {};
+    const categoryService: Partial<CategoryService> = {};
+    const activatedRouteStub: Partial<ActivatedRoute> = {
+        data: new Observable<Data>()
+    };
+    const carrierService: Partial<CarrierService> = {};
+    const usersServiceStub: Partial<UsersService> = {
+        getCurrentUsers(): Observable<any> {
+            return new Observable<any>();
+        }
+    };
+    const transaktionService: Partial<TransaktionService> = {};
     const router: Partial<Router> = {};
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [BookingViewComponent],
-            imports: [FormsModule, LocalizationModule.forRoot(l10nConfig), MatDialogModule, TerraComponentsModule],
+            imports: [
+                FormsModule,
+                LocalizationModule.forRoot(l10nConfig),
+                MatDialogModule,
+                TerraComponentsModule,
+                MatTableModule,
+                MatCheckboxModule,
+                MatExpansionModule,
+                MatFormFieldModule,
+                MatSelectModule,
+                MatRadioModule,
+                BrowserAnimationsModule
+            ],
             providers: [
                 {
-                    provide: LoginService,
-                    useValue: loginServiceStub
+                    provide: CategoryService,
+                    useValue: categoryService
+                },
+                {
+                    provide: TransaktionService,
+                    useValue: transaktionService
+                },
+                {
+                    provide: CarrierService,
+                    useValue: carrierService
                 },
                 {
                     provide: UsersService,
@@ -35,6 +74,10 @@ describe('Component: AppLoginComponent', () => {
                 {
                     provide: Router,
                     useValue: router
+                },
+                {
+                    provide: ActivatedRoute,
+                    useValue: activatedRouteStub
                 },
                 WbsSitemapHelper,
                 GlobalRegistryService,
@@ -49,5 +92,8 @@ describe('Component: AppLoginComponent', () => {
         fixture.detectChanges();
     });
 
-
+    it('should create an instance', () =>
+    {
+        expect(component).toBeTruthy();
+    });
 });
